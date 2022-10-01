ef594840b6447060e6c0bfc6ca613bd9bd6ffdae
SGO-NELSON\Lenovo <nelson@sgo.co.id>
2022-10-01 10:38:47 +0700

    update template jemaat baru

--- absen/resources/views/superadmin/tempuser/publishtempuserlayout.blade.php
+++ absen/resources/views/superadmin/tempuser/publishtempuserlayout.blade.php
@@ -3,13 +3,14 @@
 @section('content') 
     <div class="page-wrapper">
         <div class="container-fluid">
-           
             <div class="card p-3">  
-                <div class="row justify-content-center">
+            <h1 class="text-center mb-3">Selamat Datang di GPdI Sahabat Allah</h1>
+
+                <div class="row justify-content-center mt-3">
                     @foreach($users as $user)
                         <div class="col-3 mb-3">
                             <div class="row mb-3">
-                                <img src="{{asset('/absen/storage/app/public/'.$user->foto)}}" class="mx-auto" style="height:40vh !important; width:30vh !important; max-height: 40vh !important;max-width: 30vh !important;" alt="{{$user->foto}}">
+                                <img src="{{asset('/absen/storage/app/public/'.$user->foto)}}" class="mx-auto" style="height:50vh !important; width:100% !important; max-height: 50vh !important;max-width: 100% !important;" alt="{{$user->foto}}">
                             </div>
                             <div class="row">
                                 <span class="text-center">{{$user->name}}</span>
